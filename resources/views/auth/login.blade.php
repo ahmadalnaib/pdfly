698cffcdb25fdad2b53a02f80d72f5acb5c282ae
ahmadalnaib <alnaib888@gmail.com>
2024-03-09 16:52:57 +0100

    fixed the register and login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,67 @@
 <x-guest-layout>
-    <section >
-        <div class="max-w-5xl mx-auto">
-            <div class="flex flex-col items-center md:flex-row">
-    
-                <div class="w-full space-y-5 md:w-3/5 md:pr-16">
-                    <p class="font-medium text-blue-500 uppercase" data-primary="blue-500">ابتكارات PDF الذكية</p>
-                    <h2 class="text-2xl font-extrabold leading-none text-black sm:text-3xl md:text-5xl">
+    <section class="bg-gray-100 py-10">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex flex-col items-center md:flex-row md:space-x-16">
+
+                <div class="w-full space-y-5 md:w-1/2">
+                    <p class="font-semibold text-blue-600 uppercase">ابتكارات PDF الذكية</p>
+                    <h2 class="text-3xl font-extrabold leading-none text-black sm:text-4xl md:text-6xl">
                         إعادة تعريف التفاعل مع المستندات.
                     </h2>
-                    <p class="text-xl text-gray-600 md:pr-16">اكتشف كيف يمكن لتقنيات الذكاء الاصطناعي أن تحول تجربتك مع ملفات PDF. نحن في طليعة ثورة تجربة المستخدم والتعامل مع المستندات.</p>
-                    
+                    <p class="text-xl text-gray-700">اكتشف كيف يمكن لتقنيات الذكاء الاصطناعي أن تحول تجربتك مع ملفات PDF. نحن في طليعة ثورة تجربة المستخدم والتعامل مع المستندات.</p>
                 </div>
-    
-                <div class="w-full mt-16 md:mt-0 md:w-2/5">
-                    <div class="relative z-10 h-auto p-8 py-10 overflow-hidden bg-white border-b-2 border-gray-300 rounded-lg shadow-2xl px-7" data-rounded="rounded-lg" data-rounded-max="rounded-full">
+
+                <div class="w-full mt-10 md:mt-0 md:w-1/2">
+                    <div class="bg-white shadow overflow-hidden sm:rounded-lg p-8">
                         <x-auth-session-status class="mb-4" :status="session('status')" />
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
-                    
+
                             <!-- Email Address -->
                             <div>
                                 <x-input-label for="email" :value="__('auth.E-Mail')" />
                                 <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
                                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
                             </div>
-                    
+
                             <!-- Password -->
                             <div class="mt-4">
                                 <x-input-label for="password" :value="__('auth.Password')" />
-                    
+
                                 <x-text-input id="password" class="block mt-1 w-full"
                                                 type="password"
                                                 name="password"
                                                 required autocomplete="current-password" />
-                    
+
                                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
                             </div>
-                    
+
                             <!-- Remember Me -->
                             <div class="block mt-4">
                                 <label for="remember_me" class="inline-flex items-center">
-                                    <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                                    <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('auth.Remember me') }}</span>
+                                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-blue-500 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50" name="remember">
+                                    <span class="ml-2 text-sm text-gray-600">{{ __('auth.Remember me') }}</span>
                                 </label>
                             </div>
-                    
-                            <div class="flex items-center justify-end mt-4">
+
+                            <div class="flex items-center justify-between mt-4">
                                 @if (Route::has('password.request'))
-                                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                                         {{ __('auth.Forgot your password?') }}
                                     </a>
                                 @endif
-                    
-                                <x-primary-button class="ms-3">
+
+                                <x-primary-button class="ml-3">
                                     {{ __('auth.Log in') }}
                                 </x-primary-button>
                             </div>
                         </form> 
-                    
+
                         <p class="w-full mt-4 text-sm text-center text-gray-500">ليس لديك حساب؟ <a href="{{route('register')}}" class="text-blue-500 underline">سجل هنا</a></p>
                     </div>
                 </div>
-    
+
             </div>
         </div>
     </section>
-    
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,74 +1,55 @@
 <x-guest-layout>
-    {{-- --}}
-
-    <section class="w-full bg-white " >
-
+    <section class="w-full bg-white py-10" >
         <div class="mx-auto max-w-7xl">
-            <div class="flex flex-col lg:flex-row">
-                <div class="relative w-full bg-cover lg:w-6/12 xl:w-7/12 bg-gradient-to-r from-white via-white to-gray-100">
-                    <div class="relative flex flex-col items-center justify-center w-full h-full px-10 my-20 lg:px-16 lg:my-0">
+            <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
+                <div class="relative w-full bg-cover bg-gradient-to-r from-white via-white to-white p-10 lg:p-20">
+                    <div class="flex flex-col items-center justify-center w-full h-full">
                         <div class="flex flex-col items-start space-y-8 tracking-tight lg:max-w-3xl">
                             <div class="relative">
-                                <p class="mb-2 font-medium text-gray-700 uppercase">ابتكارات AI لتحسين ملفات PDF</p>
-
-
-                                <h2 class="text-5xl font-bold text-gray-900 xl:text-6xl">مزايا تمكنك من العمل بذكاء أعمق مع ملفات PDF</h2>
+                                <p class="mb-2 font-medium text-gray-700 uppercase text-2xl">ابتكارات AI لتحسين ملفات PDF</p>
+                                <h2 class="text-6xl font-bold text-gray-900 xl:text-7xl">مزايا تمكنك من العمل بذكاء أعمق مع ملفات PDF</h2>
                             </div>
-                            <p class="text-2xl text-gray-700">قمنا بتطوير نهج مبتكر يتيح لك استخدام الذكاء الاصطناعي لزيادة إنتاجية عملك مع ملفات PDF.</p>
-                      
-                            </div>
-                            
+                            <p class="text-3xl text-gray-700">قمنا بتطوير نهج مبتكر يتيح لك استخدام الذكاء الاصطناعي لزيادة إنتاجية عملك مع ملفات PDF.</p>
+                        </div>
                     </div>
                 </div>
-    
-                <div class="w-full bg-white lg:w-6/12 xl:w-5/12">
-                    <div class="flex flex-col items-start justify-start w-full h-full p-10 lg:p-16 xl:p-24">
-                    
+                <div class="w-full bg-white p-10 lg:p-20">
+                    <div class="flex flex-col items-start justify-start w-full h-full">
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
-                    
                             <!-- Name -->
-                            <div>
+                            <div class="mb-4">
                                 <x-input-label for="name" :value="__('auth.Name')" />
-                                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                                <x-text-input id="name" class="block mt-1 w-full text-lg" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
                                 <x-input-error :messages="$errors->get('name')" class="mt-2" />
                             </div>
-                    
                             <!-- Email Address -->
-                            <div class="mt-4">
+                            <div class="mb-4">
                                 <x-input-label for="email" :value="__('auth.E-Mail')" />
-                                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                                <x-text-input id="email" class="block mt-1 w-full text-lg" type="email" name="email" :value="old('email')" required autocomplete="username" />
                                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
                             </div>
-                    
                             <!-- Password -->
-                            <div class="mt-4">
+                            <div class="mb-4">
                                 <x-input-label for="password" :value="__('auth.Password')" />
-                    
-                                <x-text-input id="password" class="block mt-1 w-full"
+                                <x-text-input id="password" class="block mt-1 w-full text-lg"
                                                 type="password"
                                                 name="password"
                                                 required autocomplete="new-password" />
-                    
                                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
                             </div>
-                    
                             <!-- Confirm Password -->
-                            <div class="mt-4">
+                            <div class="mb-4">
                                 <x-input-label for="password_confirmation" :value="__('auth.Confirm Password')" />
-                    
-                                <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                                <x-text-input id="password_confirmation" class="block mt-1 w-full text-lg"
                                                 type="password"
                                                 name="password_confirmation" required autocomplete="new-password" />
-                    
                                 <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
                             </div>
-                    
                             <div class="flex items-center justify-end mt-4">
                                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                                     {{ __('auth.Already registered?') }}
                                 </a>
-                    
                                 <x-primary-button class="ms-4">
                                     {{ __('auth.Register') }}
                                 </x-primary-button>
@@ -78,6 +59,5 @@
                 </div>
             </div>
         </div>
-    
     </section>
 </x-guest-layout>
