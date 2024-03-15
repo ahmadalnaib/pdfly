1b8d78e0539bfb320bea6e0fe2deceee944a6c7f
ahmedhabeeb73 <alnaib888@gmail.com>
2024-03-15 22:13:44 +0100

    add fav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,21 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <meta property="og:title" content="pdfly.ai - استفسر من الذكاء الاصطناعي عن ملفات PDF" />
+<meta property="og:description" content="pdfly.ai هي أداة متقدمة تمكنك من رفع ملفات PDF والاستفسار مباشرة من الذكاء الاصطناعي للحصول على معلومات محددة دون الحاجة إلى قراءة الملف كاملاً." />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://www.pdfly.ai" />
+<meta property="og:image" content="{{ asset('fav/android-chrome-512x512') }}" />
+<meta name="description" content="استخدم pdfly.ai لاستخراج المعلومات من ملفات PDF بسهولة عبر الذكاء الاصطناعي. رفع، استفسر، واحصل على الإجابات بسرعة." />
+<meta name="keywords" content="PDF, ذكاء اصطناعي, استخراج بيانات, معالجة ملفات PDF, استفسارات PDF" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ url('fav/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ url('fav/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ url('fav/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ url('fav/site.webmanifest') }}">
+        <link rel="mask-icon" href="{{ url('fav/safari-pinned-tab.svg') }}" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
