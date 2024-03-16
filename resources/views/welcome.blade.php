56dd5dee355e577d3cbcac876c1afb8e16419ecd
ahmedhabeeb73 <alnaib888@gmail.com>
2024-03-16 09:26:30 +0100

    add twitter

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -226,7 +226,7 @@
 
                 </ul>
                 <div class="box-border right-0 flex justify-center w-full mt-4 space-x-3 border-solid md:w-auto md:justify-end md:absolute md:mt-0">
-                    <a href="" class="inline-flex items-center leading-7 text-gray-900 no-underline border-0 border-gray-200 cursor-pointer hover:text-blue-700 focus-within:text-blue-700">
+                    <a target="_blank" href="https://twitter.com/pdfly_ai" class="inline-flex items-center leading-7 text-gray-900 no-underline border-0 border-gray-200 cursor-pointer hover:text-blue-700 focus-within:text-blue-700">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
   <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
 </svg>
