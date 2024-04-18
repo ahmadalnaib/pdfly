184b51c70a0c9b74cdb336f43ac61eb20550f951
ahmad.al-naib <alnaib888@gmail.com>
2024-04-18 09:36:15 +0200

    rm fc google ads

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="google-adsense-account" content="ca-pub-3312007983311896">
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
