d9efe0256eeaa66e436d0a5e450919eb3fd38aca
ahmad.al-naib <alnaib888@gmail.com>
2024-04-25 15:43:28 +0200

    put google ad

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -6,6 +6,8 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3312007983311896"
+    crossorigin="anonymous"></script>
 
     <title>{{ config('app.name', 'PDFLY.ai') }}</title>
 
